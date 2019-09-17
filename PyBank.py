a54dd1cd1af94d1df4284f0bb4fcf91afa36dbe6
Alexis Schottenstein <32373615+KeepItOnTheDownload@users.noreply.github.com>
2019-09-17 12:59:12 -0400

    Update PyBank.py

--- PyBank.py
+++ PyBank.py
@@ -48,11 +48,6 @@ with open(budget_filepath) as budget_data:
         elif (net_change < greatest_decrease[1]):
             greatest_decrease[0] = data[0]
             greatest_decrease[1] = net_change
-# Correlate max and min to the proper month using month list and index from max and min
-    #max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-    #max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
-
-##Is this the best way to do it? bette with a Else if statement? 
 
 #Print results
 print("Financial Analysis")
@@ -63,33 +58,7 @@ print(f"Average Change:, ${round(sum(monthly_profit)/ len(monthly_profit),2)}")
 print(f"Greatest Increase in Profits: ${greatest_increase[1]} {str(greatest_increase[0])}")
 print(f"Greatest Decrease in Profits: ${greatest_decrease[1]} {str(greatest_decrease[0])}")
 
-## How do I add commos in the print fumction here? 
 
-
-#%%
-#Obtain the max and min of the the montly profit change list
-max_increase_value = max(monthly_profit_change)
-max_decrease_value = min(monthly_profit_change)
-
-# Correlate max and min to the proper month using month list and index from max and min
-max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
-
-##Is this the best way to do it? bette with a Else if statement? 
-
-#Print results
-print("Financial Analysis\n")
-print("----------------------------\n")
-print(f"Total Months: {len(data)}\n")
-print(f"Total: ${sum(total_profit)}\n")
-print(f"Average Change:, ${round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
-
-## How do I add commos in the print fumction here? 
-
-
-#%%
 
 
 
